75ca208c9ffb1b15c037229b20d665efdf7ce3fd
tawnchan <tawnchan@gmail.com>
2022-09-26 10:41:46 -0700

    add second user story

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
-import React, { useState } from "react"
-import "./App.css"
+import React, { useState } from "react";
+import "./App.css";
+import Square from "./components/Square";
 
 const App = () => {
   const [board, setBoard] = useState([
@@ -11,14 +12,30 @@ const App = () => {
     "?",
     "?",
     "?",
-    "?"
-  ])
+    "?",
+  ]);
+
+  const handleGamePlay = (clickedSquare) => {
+    alert(clickedSquare);
+  };
 
   return (
     <>
       <h1>Treasure Hunt Game</h1>
+      <div className="game_container">
+        {board.map((square, index) => {
+          return (
+            <Square
+              square={square}
+              index={index}
+              key={index}
+              handleGamePlay={handleGamePlay}
+            />
+          );
+        })}
+      </div>
     </>
-  )
-}
+  );
+};
 
-export default App
+export default App;
