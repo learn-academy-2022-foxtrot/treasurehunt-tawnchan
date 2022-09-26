d00ab6caa7a730a26bff4d71cc5404444cc68fcd
tawnchan <tawnchan@gmail.com>
2022-09-26 11:50:50 -0700

    update app.js, some reason it wasn't working

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
-import React, { useState } from "react"
-import "./App.css"
-import Square from './components/Square'
+import React, { useState } from "react";
+import Square from "./components/Square";
+import "./App.css";
 
 const App = () => {
   const [board, setBoard] = useState([
@@ -12,71 +12,56 @@ const App = () => {
     "?",
     "?",
     "?",
-    "?"
-  ])
-  const [treasureLocation, setTreasureLocation] = useState(Math.floor(Math.random() * board.length))
-  const [bombLocation, setBombLocation] = useState(Math.floor(Math.random() * board.length))
+    "?",
+  ]);
+  const [treasureLocation, setTreasureLocation] = useState(
+    Math.floor(Math.random() * board.length)
+  );
+  const [bombLocation, setBombLocation] = useState(
+    Math.floor(Math.random() * board.length)
+  );
 
   const handleGamePlay = (clickedSquare) => {
-    let updateBoard = [...board]
-    if(clickedSquare === treasureLocation) {
-      updateBoard[clickedSquare] = "💎"
-      setBoard(updateBoard)
-    } else if(clickedSquare === bombLocation) {
-      updateBoard[clickedSquare] = "💣"
-      setBoard(updateBoard)
+    console.log("treasureLocation:", treasureLocation);
+    console.log("bombLocation:", bombLocation);
+    let updateBoard = [...board];
+    if (clickedSquare === treasureLocation) {
+      updateBoard[clickedSquare] = "💎";
+      setBoard(updateBoard);
+    } else if (clickedSquare === bombLocation) {
+      updateBoard[clickedSquare] = "💣";
+      setBoard(updateBoard);
     } else {
-      updateBoard[clickedSquare] = "🌴"
-
-    setBoard(updateBoard)
+      updateBoard[clickedSquare] = "🌴";
+      setBoard(updateBoard);
     }
-  }
+  };
 
   const handleReset = () => {
-    setBoard([
-      "?",
-      "?",
-      "?",
-      "?",
-      "?",
-      "?",
-      "?",
-      "?",
-      "?"
-    ])
-    setTreasureLocation(Math.floor(Math.random() * board.length))
-    setBombLocation(Math.floor(Math.random() * board.length))
-  }
-      setBoard(updateBoard)
-    }
-  }
+    setBoard(["?", "?", "?", "?", "?", "?", "?", "?", "?"]);
+    setTreasureLocation(Math.floor(Math.random() * board.length));
+    setBombLocation(Math.floor(Math.random() * board.length));
+  };
 
-  console.log("treasureLocation:", treasureLocation)
-  console.log("bombLocation:", bombLocation)
+  console.log("treasureLocation:", treasureLocation);
   return (
     <>
       <h1>Treasure Hunt Game</h1>
       <div className="board-game">
         {board.map((square, index) => {
           return (
-          <Square 
-          square={square} 
-          index={index} 
-          key={index}
-          handleGamePlay={handleGamePlay} />
-        )
             <Square
               square={square}
               index={index}
               key={index}
               handleGamePlay={handleGamePlay}
             />
-          )
+          );
         })}
       </div>
-      <button onClick={handleReset}>Restart Game</button>
+      <button onClick={handleReset}>Play Again</button>
     </>
-  )
-}
+  );
+};
 
-export default App
+export default App;
