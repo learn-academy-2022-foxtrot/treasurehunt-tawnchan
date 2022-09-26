23083488d2b768cde6c8473f2e2b933a41b72922
tawnchan <tawnchan@gmail.com>
2022-09-26 11:36:53 -0700

    added functioning reset button

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, { useState } from "react"
-import Square from "./components/Square"
 import "./App.css"
+import Square from './components/Square'
 
 const App = () => {
   const [board, setBoard] = useState([
@@ -14,36 +14,57 @@ const App = () => {
     "?",
     "?"
   ])
-
   const [treasureLocation, setTreasureLocation] = useState(Math.floor(Math.random() * board.length))
+  const [bombLocation, setBombLocation] = useState(Math.floor(Math.random() * board.length))
 
   const handleGamePlay = (clickedSquare) => {
+    // makes a copy of the board in state so  that we can modify it in this method
     let updateBoard = [...board]
     if(clickedSquare === treasureLocation) {
-      updateBoard[clickedSquare] = '♛'
+      updateBoard[clickedSquare] = "💎"
+      setBoard(updateBoard)
+    } else if(clickedSquare === bombLocation) {
+      updateBoard[clickedSquare] = "💣"
       setBoard(updateBoard)
     } else {
-    updateBoard[clickedSquare] = '🌴'
+      updateBoard[clickedSquare] = "🌴"
     setBoard(updateBoard)
     }
   }
 
-  console.log("treasureLocation", treasureLocation)
+  const handleReset = () => {
+    setBoard([
+      "?",
+      "?",
+      "?",
+      "?",
+      "?",
+      "?",
+      "?",
+      "?",
+      "?"
+    ])
+    setTreasureLocation(Math.floor(Math.random() * board.length))
+    setBombLocation(Math.floor(Math.random() * board.length))
+  }
+
+  console.log("treasureLocation:", treasureLocation)
+  console.log("bombLocation:", bombLocation)
   return (
     <>
       <h1>Treasure Hunt Game</h1>
-      <div className = "board-game">
+      <div className="board-game">
         {board.map((square, index) => {
           return (
-          <Square square = {square}
-                  index = {index}
-                  key = {index}
-                  handleGamePlay = {handleGamePlay}
-                  />
-          )
+          <Square 
+          square={square} 
+          index={index} 
+          key={index}
+          handleGamePlay={handleGamePlay} />
+        )
         })}
-
       </div>
+      <button onClick={handleReset}>Restart Game</button>
     </>
   )
 }
